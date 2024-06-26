287803d03f53db5a8be87531b9dd5e39b560e123
Luigi D'Angelo <l.dangelo@ecitalia.it>
2024-06-26 17:07:20 +0200

    inizio form client

---
+++ Client/MainForm.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Client
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var checkedItems = checkedListBox1.CheckedItems;
+
+            foreach (var serverIp in checkedItems)
+            {
+                int port = 5000;
+
+                Console.Write("Inserisci il messaggio da inviare: ");
+                //string message = Console.ReadLine();
+                string message = "test";
+
+                SendMessage(serverIp.ToString(), port, message);
+
+                //TcpClient client = new TcpClient("192.168.168.38", 5000);
+                //NetworkStream stream = client.GetStream();
+
+                //string message = "Ciao, server!";
+                //byte[] data = Encoding.ASCII.GetBytes(message);
+                //stream.Write(data, 0, data.Length);
+
+                //byte[] buffer = new byte[1024];
+                //int bytesRead = stream.Read(buffer, 0, buffer.Length);
+                //string response = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+                //Console.WriteLine("Risposta: " + response);
+
+                //stream.Close();
+                //client.Close();
+            }
+        }
+        private static void SendMessage(string serverIp, int port, string message)
+        {
+            TcpClient client = new TcpClient(serverIp, port);
+            NetworkStream stream = client.GetStream();
+
+            byte[] buffer = Encoding.UTF8.GetBytes(message);
+            stream.Write(buffer, 0, buffer.Length);
+
+            stream.Close();
+            client.Close();
+
+            Console.WriteLine("Messaggio inviato!");
+        }
+    }
+}
