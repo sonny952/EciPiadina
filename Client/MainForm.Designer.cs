4918c67e62a9133cb5c117905f6a416c1d954660
Luigi D'Angelo <l.dangelo@ecitalia.it>
2024-06-27 09:06:50 +0200

    pull

--- Client/MainForm.Designer.cs
+++ Client/MainForm.Designer.cs
@@ -28,8 +28,14 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // checkedListBox1
@@ -46,7 +52,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(388, 88);
+            this.button1.Location = new System.Drawing.Point(368, 83);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
@@ -54,11 +60,43 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // contextMenuStrip2
+            // 
+            this.contextMenuStrip2.Name = "contextMenuStrip2";
+            this.contextMenuStrip2.Size = new System.Drawing.Size(61, 4);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(515, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(224, 200);
+            this.richTextBox1.TabIndex = 6;
+            this.richTextBox1.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(368, 269);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.checkedListBox1);
             this.Name = "MainForm";
@@ -71,5 +109,9 @@
 
         private System.Windows.Forms.CheckedListBox checkedListBox1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
     }
 }
--- Client/MainForm.cs
+++ Client/MainForm.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Net.Sockets;
 using System.Text;
@@ -24,42 +25,97 @@ namespace Client
 
             foreach (var serverIp in checkedItems)
             {
-                int port = 5000;
+                Task.Run(async () =>
+                {
+                    int port = 5000;
+                    using (TcpClient client = new TcpClient(serverIp.ToString(), port))
+                    using (NetworkStream stream = client.GetStream())
+                    {
+                        Console.Write("Inserisci il messaggio da inviare: ");
+                        string message = "test";
 
-                Console.Write("Inserisci il messaggio da inviare: ");
-                //string message = Console.ReadLine();
-                string message = "test";
-
-                SendMessage(serverIp.ToString(), port, message);
-
-                //TcpClient client = new TcpClient("192.168.168.38", 5000);
-                //NetworkStream stream = client.GetStream();
-
-                //string message = "Ciao, server!";
-                //byte[] data = Encoding.ASCII.GetBytes(message);
-                //stream.Write(data, 0, data.Length);
-
-                //byte[] buffer = new byte[1024];
-                //int bytesRead = stream.Read(buffer, 0, buffer.Length);
-                //string response = Encoding.ASCII.GetString(buffer, 0, bytesRead);
-                //Console.WriteLine("Risposta: " + response);
-
-                //stream.Close();
-                //client.Close();
+                        var msg = await SendMessageAsync(serverIp.ToString(), port, message, client, stream);
+                        AppendTextToRichTextBox(msg + "-");
+                    }
+                });
             }
         }
-        private static void SendMessage(string serverIp, int port, string message)
+
+        private static async Task<string> SendMessageAsync(string serverIp, int port, string message, TcpClient client, NetworkStream stream)
         {
-            TcpClient client = new TcpClient(serverIp, port);
-            NetworkStream stream = client.GetStream();
+            try
+            {
+                // Send the message to the server
+                byte[] buffer = Encoding.UTF8.GetBytes(message);
+                await stream.WriteAsync(buffer, 0, buffer.Length);
+                Console.WriteLine("Messaggio inviato!");
 
-            byte[] buffer = Encoding.UTF8.GetBytes(message);
-            stream.Write(buffer, 0, buffer.Length);
+                // Buffer for reading the response
+                byte[] responseBuffer = new byte[1024];
+                int bytesRead = await stream.ReadAsync(responseBuffer, 0, responseBuffer.Length);
+                string response = Encoding.UTF8.GetString(responseBuffer, 0, bytesRead);
 
-            stream.Close();
-            client.Close();
+                // Display the response
+                Console.WriteLine("Risposta dal server: " + response);
+                return response;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Errore: " + ex.Message);
+                return "Errore: " + ex.Message;
+            }
+        }
 
-            Console.WriteLine("Messaggio inviato!");
+        private void AppendTextToRichTextBox(string text)
+        {
+            if (InvokeRequired)
+            {
+                Invoke(new Action<string>(AppendTextToRichTextBox), new object[] { text });
+                return;
+            }
+
+            richTextBox1.Text += text;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "test.csv");
+            var text = richTextBox1.Text;
+            CreateCsvFile(filePath, text);
+
+            if (File.Exists(filePath))
+            {
+                SaveFileDialog saveFileDialog = new SaveFileDialog
+                {
+                    FileName = "test.csv",
+                    Filter = "CSV files (*.csv)|*.csv|All files (*.*)|*.*",
+                    DefaultExt = "csv",
+                    AddExtension = true
+                };
+
+                File.Copy(filePath, saveFileDialog.FileName, true);
+                MessageBox.Show($"CSV file downloaded to: {saveFileDialog.FileName}");
+
+            }
+            else
+            {
+                MessageBox.Show("CSV file does not exist. Please create it first.");
+            }
+        }
+
+        private void CreateCsvFile(string filePath, string text)
+        {
+            StringBuilder csvContent = new StringBuilder();
+
+            var array = text.Split('-');
+
+            foreach( var item in array )
+            {
+                csvContent.AppendLine(item);
+            }
+
+
+            File.WriteAllText(filePath, csvContent.ToString());
         }
     }
 }
